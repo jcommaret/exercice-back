72e73289094f1b8139dd5cd268f67a284f43f1ae
Jérôme Commaret <jcommaret@gmail.com>
2021-04-16 13:20:59 +0200

    migration added to contracts

--- db/migrate/20210415202131_create_contracts.rb
+++ db/migrate/20210415202131_create_contracts.rb
@@ -7,7 +7,7 @@ class CreateContracts < ActiveRecord::Migration[6.1]
       t.date "date_start"
       t.date "date_end"
       t.boolean "all_risk"
-
+      t.belongs_to :users
       t.timestamps
     end
   end
